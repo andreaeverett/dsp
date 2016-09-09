2cb4ff7baab94a81fd9664795281e541193a7d2f
Andrea Everett <aeverett@Andrea-Everetts-MacBook-Air.local>
2016-09-08 20:38:54 -0700

    part 2 done!

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,20 @@
-PLACE YOUR CODE HERE
+
+# Code Repeated from Part 1:
+
+import csv
+import re
+
+faculty_members = csv.DictReader(open('faculty.csv'))
+
+emails = []
+
+for member in faculty_members:
+    email = re.search( r'(.+)@(.+)', member[' email'])
+    emails.append(email.group(1) + '@' + email.group(2))
+
+
+# New Code Part 2
+
+with open('emails.csv', 'w') as f:
+    emailwriter = csv.writer(f, delimiter = '\n')
+    emailwriter.writerow(emails)
